0472940b291c1fd4c03032fd9cd374ee96fbfd81
Marco Santana <marcos.sinsan@gmail.com>
2021-11-13 12:59:58 -0300

    Alterando Cadastro controller

--- m4u-bd-node/src/controller/CadastroController.ts
+++ m4u-bd-node/src/controller/CadastroController.ts
@@ -7,8 +7,19 @@ import { Request, Response } from "express";
 export default {
     async create(req: Request, res: Response) {
         const {nome, cpf} = req.body;
-        const id = 3;
+        const id = 999;
         const data = {id, nome, cpf};
         return res.status(201).json({data:data});
+    },
+    async list(req: Request, res: Response){
+        var result = [{id: 2, cpf:"123456789", nome: "Fulano de Tal"}, {id:1, cpf: "987654321", nome: "Ciclano de TAl"}];
+        return res.status(200).json({data:result});
+    },
+    async update(req: Request, res: Response) {
+        const {nome, cpf} = req.body;
+        const dataAlteracao = '13/11/2021 12:50';
+        const cadastro = {nome, cpf, dataAlteracao};
+        cadastro.nome = "João do pé de feijão";
+        return res.status(200).json({data:cadastro});
     }
 }
--- m4u-bd-node/src/server.ts
+++ m4u-bd-node/src/server.ts
@@ -1,11 +1,12 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
+
 // Método que recebe como param a porta e uma func callback
 app.listen(3000, () => console.log("Servidor inicializando na porta 3000..."));
-
-// Método get
-app.get("/", (request, response) => {
-    return response.json({mensagem: "Bem-vindos ao gama - xp", ano:2021});
-});
