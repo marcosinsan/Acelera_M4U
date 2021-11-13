815f8fd688eb3f1071ec0df95a928dec847bea21
Marco Santana <marcos.sinsan@gmail.com>
2021-11-13 12:11:05 -0300

    Criando diretorio Controller

--- m4u-bd-node/src/server.ts
+++ m4u-bd-node/src/server.ts
@@ -1,6 +1,11 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
 // Método que recebe como param a porta e uma func callback
 app.listen(3000, () => console.log("Servidor inicializando na porta 3000..."));
+
+// Método get
+app.get("/", (request, response) => {
+    return response.json({mensagem: "Bem-vindos ao gama - xp", ano:2021});
+});
