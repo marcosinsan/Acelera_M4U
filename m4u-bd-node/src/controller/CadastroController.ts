0d282853d5a4bd8cc427ca66ce551f0ccdaa4ded
Marco Santana <marcos.sinsan@gmail.com>
2021-11-15 12:46:36 -0300

    Update de CadastroController e as suas rotas.

--- m4u-bd-node/src/controller/CadastroController.ts
+++ m4u-bd-node/src/controller/CadastroController.ts
@@ -3,23 +3,50 @@
 // Imports
 import { Request, Response } from "express";
 
+import knex from "../database/connection";
+
 // Recurso de requisição
 export default {
+
     async create(req: Request, res: Response) {
         const {nome, cpf} = req.body;
-        const id = 999;
-        const data = {id, nome, cpf};
+        
+        const data = {cpf, nome};
+
+        await knex('tab_cadastro').insert(data);
+        
         return res.status(201).json({data:data});
     },
+
     async list(req: Request, res: Response){
-        var result = [{id: 2, cpf:"123456789", nome: "Fulano de Tal"}, {id:1, cpf: "987654321", nome: "Ciclano de TAl"}];
+        var result = await knex('tab_cadastro').orderBy('id');
         return res.status(200).json({data:result});
     },
+
+    async find(req: Request, res: Response){
+        const { id } = req.params;
+        const user = await knex('tab_cadastro').where({ id });
+        return res.status(200).json(user);
+    },
+
     async update(req: Request, res: Response) {
+        
+        const { id } = req.params;
         const {nome, cpf} = req.body;
-        const dataAlteracao = '13/11/2021 12:50';
-        const cadastro = {nome, cpf, dataAlteracao};
-        cadastro.nome = "João do pé de feijão";
+
+        const data = {cpf, nome};
+
+        await knex('tab_cadastro').update(data).where({ id });
+
+        const cadastro = await knex('tab_cadastro').where({ id });
+
         return res.status(200).json({data:cadastro});
+    }, 
+
+    async delete(req: Request, res: Response) {
+        const { id } = req.params;
+
+        await knex('tab_cadastro').delete().where({ id });
+        return res.status(200).json({message: "Resgistro excluido com Sucesso!"})
     }
 }
--- m4u-bd-node/src/routes.ts
+++ m4u-bd-node/src/routes.ts
@@ -5,7 +5,10 @@ import CadastroController from "./controller/CadastroController";
 const routes = Router();
 
 routes.post("/cadastros", CadastroController.create);
-routes.put("/cadastros", CadastroController.update);
+routes.put("/cadastros/:id", CadastroController.update);
 routes.get("/cadastros", CadastroController.list);
+routes.get("/cadastros/:id", CadastroController.find);
+routes.delete("/cadastros/:id", CadastroController.delete);
+
 
 export default routes;
